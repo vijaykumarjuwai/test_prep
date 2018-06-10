456e0f7d7b4e1ec9a36407334a65a5a81e10a964
Charles Carrier <charles.e.carrier@gmail.com>
2018-06-10 08:27:02 +0000

    Adding content to github

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuestionsComponent } from './questions/questions.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'questions',
+  component: QuestionsComponent,
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionsComponent } from './questions/questions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionsComponent
   ],
   imports: [
     BrowserModule,
